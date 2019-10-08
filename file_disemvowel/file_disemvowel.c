fab852c4f33b68f49d996b7dbf3be7cfec701ca8
JakePeterson7 <jake.peterson7@aol.com>
2019-10-08 11:34:02 -0500

    Initialize file_disemvowel.c/h
    We built main and the disemvowel(in, out) function. Still need to build the other functions.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -7,70 +7,83 @@
 
 #define BUF_SIZE 1024
 
-char *disemvowel(char *str) {
-
-   char* newName;
-   int len;
-   int nameCount;
-   int i;
-   nameCount = 0;
-   len = strlen(str);
-   newName = (char*) calloc(len+1, sizeof(char));
-
-
-   for (i=0; i<len; ++i) {
-      if (c == 'a' || c == 'e' || c == 'i' || c == 'o' ||
-       c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-      {
-      //do nothing
-      }
-      //add consonants to new char* newName
-      else{bool is_vowel(char c) {
-    /*
-     * Returns true if c is a vowel (upper or lower case), and
-     * false otherwise.
-     */
-      newName[nameCount] = c;
-      nameCount++;
-      }
-   }
-
-   newName[len] = '\0';
-
-  return (char*) newName;
-}
 bool is_vowel(char c){
     if (c == 'a' || c == 'e' || c == 'i' || c == 'o' ||
-           c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-          {
+        c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
           //return true if c is a vowel
           return true;
           }
     return false;
 }
-int copy_non_vowels(int num_chars, char* in_buf, char* out,buf){
-    if(is_vowel == false){
 
+char *disemvowel_line(char *str) {
+
+    char* newName;
+    int len;
+    int nameCount;
+    int i;
+    nameCount = 0;
+    len = strlen(str);
+    newName = (char*) calloc(len+1, sizeof(char));
+
+    for (i=0; i<len; ++i) {
+        if (is_vowel(str[i])){
+            //do nothing
+        } else {  //add consonants to new char* newName
+            newName[nameCount] = str[i];
+            nameCount++;
+        }
     }
-    }
+
+    newName[len] = '\0';
+    return (char*) newName;
 }
+
 void disemvowel(FILE* inputFile, FILE* outputFile){
-      char* buffer;
-      size_t buffer_size = 100;
-      char* str;
+    char* buffer;
+    size_t buffer_size = 100;
+    char* str;
 
-      while(getline(&buffer, &buffer_size, stdin) > 0){
-          str = disemvowel(buffer);
-          fwrite(str, strln(str), sizeof(str), outputFile);
-      }
+    printf("in disemvowel \n");
 
+    while(getline(&buffer, &buffer_size, inputFile) > 0){
+        str = disemvowel_line(buffer);
+        fwrite(str, 1, strlen(str), outputFile);
+    }
 }
+
 int main(int argc, char* argv[]){
+
+    printf(argv[0]);
+    printf("\n");
+    printf(argv[1]);
+    printf("\n");
+    printf(argv[2]);
+    printf("\n");
 
     FILE *inputFile;
     FILE *outputFile;
 
+    if(argc == 1) {
+        inputFile = stdin;
+        outputFile = stdout;
+        printf("1 argc \n");
+    } else if (argc == 2) {
+        printf("2 argc \n");
+        inputFile = fopen(argv[1], "r");
+        outputFile = stdout;
+    } else {
+        printf("else case");
+    }
+
+    printf("out of if \n");
+
     disemvowel(inputFile, outputFile);
+
+    printf("past disemvowel \n");
+
+    fclose(inputFile);
+    fclose(outputFile);
 
     return 0;
 }
